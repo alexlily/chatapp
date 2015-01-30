e308a9e530801bf019169c6756a778a5d2f6bf48
Alex Lin <alexlily@berkeley.edu>
2015-01-30 00:10:33 -0800

    more updates to layout

--- app/src/main/java/com/example/alexlily/chatapp/ContactPageActivity.java
+++ app/src/main/java/com/example/alexlily/chatapp/ContactPageActivity.java
@@ -2,6 +2,7 @@ package com.example.alexlily.chatapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ContactPageActivity extends Activity {
@@ -34,14 +36,18 @@ public class ContactPageActivity extends Activity {
         Button contactButton;
 
         // fake data
-        String[] contactList = {getString(R.string.test_receiver)};
-
+        String[] contactList = {getString(R.string.test_receiver), "frodo", "sam"};
+        TextView title = (TextView) findViewById(R.id.contactTitle);
+        title.setText(username);
         LinearLayout contactArea = (LinearLayout)findViewById(R.id.contactList);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
+        lp.setMargins(5,5,5,5);
         for (String person: contactList){
             contactButton = new Button(this);
             contactButton.setText(person);
+            contactButton.setTextColor(Color.BLACK);
+            contactButton.setTextSize(32);
+            contactButton.setBackgroundResource(R.drawable.contact_page_entry);
             contactButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
